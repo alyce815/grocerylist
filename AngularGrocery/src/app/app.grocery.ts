4541e893023d95412d67e3a1352664a446b0e577
Alyce <alyce815@yahoo.com>
2019-03-11 14:12:16 -0700

    Committed boilerplate and initial grocery list component

---
+++ AngularGrocery/src/app/app.grocery.ts
@@ -0,0 +1,8 @@
+import {  Component } from '@angular/core';
+@Component({
+    selector: 'app-grocery',
+    templateUrl: './app.grocery.html',
+    styleUrls: [
+        '../assets/css/bootstrap.min.css'
+    ]
+})
